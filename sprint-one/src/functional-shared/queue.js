d5946c5a5fb1aaa89ae1923e68c375f3cd4d135f
iandeboisblanc <iandeboisblanc@gmail.com>
2016-01-13 15:23:20 -0800

    Successfully implements queue fuctional-sharing data structure

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.queueSize = 0;
+  newQueue.topSpot = 0;
+  newQueue.storage = {};
+  _.extend(newQueue, queueMethods);
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.topSpot] = value;
+    this.topSpot++;
+    this.queueSize++;
+  },
+  dequeue: function(){
+    var result = this.storage[this.topSpot - this.queueSize];
+    delete this.storage[this.topSpot - this.queueSize];
+    this.queueSize--;
+    return result;
+  },
+  size: function(){
+    if(this.queueSize >= 0){
+      return this.queueSize;
+    }
+    else{
+      return 0;
+    }
+  },
+};
 
 
