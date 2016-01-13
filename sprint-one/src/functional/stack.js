3d5089a87b39314dc925b69ef99dd773626dea6e
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-01-13 12:11:29 -0800

    Allow push to add elements to stack, and pop to return element at top of stack. Storage is now an array instead of an object

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,18 @@ var Stack = function() {
   var stackSize = 0;
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = [];
 
   // Implement the methods below
   someInstance.push = function(value) {
     stackSize++;
+    storage.push(value);
   };
 
   someInstance.pop = function() {
     if(stackSize > 0){
       stackSize--;
+      return storage.pop();
     }
   };
 
