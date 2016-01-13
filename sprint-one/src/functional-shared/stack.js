78d6cfc84a2162d46e504080926d48f109947961
iandeboisblanc <iandeboisblanc@gmail.com>
2016-01-13 15:11:09 -0800

    Successfully implements stack functional-shared data structure

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.stackSize = 0;
+  newStack.storage = {};
+  _.extend(newStack,stackMethods);
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+
+  pop: function(){
+    this.stackSize--;
+    var result = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    return result;
+  },
+  size: function(){
+    if(this.stackSize >= 0){
+      return this.stackSize;
+    }
+    else{
+      return 0;
+    }
+  }
+};
 
 
