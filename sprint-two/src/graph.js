3d75b75872fb3a161f1fa8776b1e7a7f12ecca0f
iandeboisblanc <iandeboisblanc@gmail.com>
2016-01-14 11:05:40 -0800

    Allow user to add and remove nodes in graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,21 +3,25 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+  this.storage = {};
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.storage[node] = [];
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return node in this.storage;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.storage[node];
 };
 
 // ------------------------
