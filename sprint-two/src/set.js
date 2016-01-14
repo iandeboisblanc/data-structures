73ae9c4da80b9285a7a773ddda8c07bcd3121eea
iandeboisblanc <iandeboisblanc@gmail.com>
2016-01-14 11:30:08 -0800

    Allows user to add values to set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,15 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  for(var key in this._storage){
+    if(key){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 };
 
 setPrototype.remove = function(item) {
