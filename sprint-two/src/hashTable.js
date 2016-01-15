3e913c4218722c6f496d77bef3719cd3a2726cbd
iandeboisblanc <iandeboisblanc@gmail.com>
2016-01-15 12:22:50 -0800

    Successfully implements doubling and halving hash table size when appropriate

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,41 +8,24 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  var alreadyUsed = false;
   this._contentCount = this._contentCount + 1;
-  if(this._storage.get(index) === undefined){
-    var bucket = [];
-    this._storage.set(index, bucket);
-  }
-  var tuple = [k,v];
-  var currentBucket = this._storage.get(index);
-  currentBucket.push(tuple);
-  if(this._contentCount > this.determineThreshold(this._limit)){
-    //create new storage array
+  if(this._contentCount > Math.floor(0.75 * this._limit)){
     this._limit = this._limit * 2;
     var newStorage = LimitedArray(this._limit);
-    //iterate through old array
-    // this._storage.each(function(valueObj, index, hashTab){
-    //   for(var key in valueObj){
-    //     // key = first names, valueObj[key] = last names/values
-    //     //rehash keys
-    //     index = getIndexBelowMaxForKey(key, this._limit);
-    //     if(this._storage.get(index) === undefined){
-    //       var hashObj = {};
-    //       hashObj[key] = valueObj[key];
-    //       this._storage.set(index, hashObj);
-    //     }
-    //     else{
-    //       var objToAddTo = this._storage.get(index);
-    //       objToAddTo[key] = valueObj[key];
-    //     }
-    //     // newStorage.set(index, hashObj);
-    //     // newStorage.set(getIndexBelowMaxForKey(item[firstName], this._limit), item);
-    //   }
-    // });
+    //loop through old array indicies
+    this._storage.each(function(bucket){
+      //this._storage.get(indices), this will return a bucket
+      _.each(bucket, function(tuple){
+        //create new hash from tuple[0]
+        var newIndex = getIndexBelowMaxForKey(tuple[0], this._limit);
+        //newStorage.set(new hash, tuple[1]), but doesnt necessarily have buckets yet
+        storageSetter(newStorage, newIndex, tuple[0], tuple[1]);
+      });
+    });
     this._storage = newStorage;
   }
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  storageSetter(this._storage, index, k, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -66,21 +49,32 @@ HashTable.prototype.remove = function(k) {
     }
   });
   this._contentCount = this._contentCount - 1;
-  if(this._contentCount <= this.determineThreshold(this._limit / 2)){
-    //create new storage array
+  if(this._contentCount < Math.floor(0.25 * this._limit)){
     this._limit = this._limit / 2;
     var newStorage = LimitedArray(this._limit);
-    //iterate through old array
-    this._storage.each(function(item,index){
-      newStorage.set(index, item);
+    //loop through old array indicies
+    this._storage.each(function(bucket){
+      //this._storage.get(indices), this will return a bucket
+      _.each(bucket, function(tuple){
+        //create new hash from tuple[0]
+        var newIndex = getIndexBelowMaxForKey(tuple[0], this._limit);
+        //newStorage.set(new hash, tuple[1]), but doesnt necessarily have buckets yet
+        storageSetter(newStorage, newIndex, tuple[0], tuple[1]);
+      });
     });
     this._storage = newStorage;
   }
 };
 
-HashTable.prototype.determineThreshold = function(limit) {
-  return Math.floor(0.75 * limit);
-};
+function storageSetter(storage, index, k, v){
+  if(storage.get(index) === undefined){
+    var bucket = [];
+    storage.set(index, bucket);
+  }
+  var tuple = [k,v];
+  var currentBucket = storage.get(index);
+  currentBucket.push(tuple);
+}
 
 
 /*
