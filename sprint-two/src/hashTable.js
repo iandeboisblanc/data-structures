4a13868e9f48e753f4febffb3b5b1973fe96cd2c
iandeboisblanc <iandeboisblanc@gmail.com>
2016-01-14 19:33:01 -0800

    Hash table successfully returns double the size when needed.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,13 +3,17 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._contentCount = 0;
+  this._threshold = 6;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   // var isIn = index in this._storage;
   var alreadyUsed = false;
-
+  this._contentCount = this._contentCount + 1;
+  var gsadf = this._contentCount;
+  //debugger;
   if(this._storage.get(index) === undefined){
     var hashObj = {};
     hashObj[k] = v;
@@ -18,6 +22,17 @@ HashTable.prototype.insert = function(k, v) {
   else{
     var objToAddTo = this._storage.get(index);
     objToAddTo[k] = v;
+  }
+  if(this._contentCount > this.determineThreshold(this._limit)){
+    //create new storage array
+    this._limit = this._limit * 2;
+    var newStorage = LimitedArray(this._limit);
+    //iterate through old array
+    for(var i = 0; i < this._limit; i++){
+      //copy values to new array
+      newStorage[i] = this._storage[i];
+    }
+    this._storage = newStorage;
   }
 };
 
@@ -30,8 +45,12 @@ HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var objToEdit = this._storage.get(index);
   delete objToEdit[k];
+  //remove object entirely if empty
 };
 
+HashTable.prototype.determineThreshold = function(limit) {
+  return Math.floor(0.75 * limit);
+};
 
 
 /*
