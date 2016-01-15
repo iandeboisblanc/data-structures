da498c1bf60d256b4fbee37f9499df91d232deaf
iandeboisblanc <iandeboisblanc@gmail.com>
2016-01-15 11:26:50 -0800

    Constructor function creates instance of binary search tree with insert, contains, and depthFirstLog functions

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,30 @@
 var BinarySearchTree = function(value) {
+
+  var newTree = Object.create(BinarySearchTree.prototype);
+
+  //properties: left and right set to undefined
+  newTree.left = undefined;
+  newTree.right = undefined;
+
+  //value equal to value
+  newTree.value = value;
+
+  //return binary search tree instance
+  return newTree;
+
 };
 
+BinarySearchTree.prototype.insert = function() {
 
+};
+
+BinarySearchTree.prototype.contains = function() {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
