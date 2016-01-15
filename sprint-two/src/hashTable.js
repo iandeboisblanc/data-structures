065a155999799b10793ba5926b4a88de40df67f7
iandeboisblanc <iandeboisblanc@gmail.com>
2016-01-14 16:33:44 -0800

    Handles hash collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,9 +7,18 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var hashObj = {};
-  hashObj[k] = v;
-  this._storage.set(index, hashObj);
+  // var isIn = index in this._storage;
+  var alreadyUsed = false;
+
+  if(this._storage.get(index) === undefined){
+    var hashObj = {};
+    hashObj[k] = v;
+    this._storage.set(index, hashObj);
+  }
+  else{
+    var objToAddTo = this._storage.get(index);
+    objToAddTo[k] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
